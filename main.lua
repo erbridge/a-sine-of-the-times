751b41e59ce40fda4018abdcf9351daec1eda6ea
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2016-04-27 13:29:57 +0100

    Set up levels and refactor

--- main.lua
+++ main.lua
@@ -1,35 +1,64 @@
-local wave_json_file = io.open("data/waves.json", "r")
-local wave_json = wave_json_file:read("*all")
+function load_json(filename)
+  local json_file = io.open(filename, "r")
+  local json = json_file:read("*all")
 
-wave_json_file:close()
+  json_file:close()
 
-local win = am.window{
-  title = "A Sine of the Times",
-}
+  return am.parse_json(json)
+end
 
-win.scene = am.rect(
-  -win.pixel_width / 2, -win.pixel_height / 2,
-  win.pixel_width / 2, win.pixel_height / 2
-)
+function setup_scene()
+  local window = am.window{
+    title = "A Sine of the Times",
+  }
 
-local level_state = {
-  start_time = 0,
-  waves = am.parse_json(wave_json)
-}
+  window.scene = am.rect(
+    -window.pixel_width / 2, -window.pixel_height / 2,
+    window.pixel_width / 2, window.pixel_height / 2
+  )
 
-win.scene:action(function(scene)
-  local t = am.frame_time - level_state.start_time
-  local value = 1
+  return window
+end
 
-  for i = 1, table.getn(level_state.waves) do
-    local wave = level_state.waves[i]
+function load_level(index)
+  local waves = load_json("data/waves.json")
+  local levels = load_json("data/levels.json")
 
-    value = value * math.sin(2 * math.pi * wave.frequency * t + wave.phase)
+  local level = {
+    index = index,
+  }
+
+  local level_waves = levels[level.index].waves
+
+  level.waves = {}
+
+  for i = 1, table.getn(level_waves) do
+    level.waves[i] = waves[level_waves[i]]
   end
 
-  if value > 0 then
-    scene.hidden = false
-  else
-    scene.hidden = true
-  end
-end)
+  return level
+end
+
+function start_level(window, level, start_time)
+  window.scene:action(function(scene)
+    local t = am.frame_time - start_time
+    local value = 1
+
+    for i = 1, table.getn(level.waves) do
+      local wave = level.waves[i]
+
+      value = value * math.sin(2 * math.pi * wave.frequency * t + wave.phase)
+    end
+
+    if value > 0 then
+      scene.hidden = false
+    else
+      scene.hidden = true
+    end
+  end)
+end
+
+local window = setup_scene()
+local level = load_level(1)
+
+start_level(window, level, am.frame_time)
